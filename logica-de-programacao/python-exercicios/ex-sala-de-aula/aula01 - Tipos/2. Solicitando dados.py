c482fd6ee18c3dadf5fe8bfb6fd066177097334a
Douglas Silva <158169029+douglass154@users.noreply.github.com>
2024-04-05 19:13:56 +0000

    Tipos em python

---
+++ logica-de-programacao/python-exercicios/ex-sala-de-aula/aula01 - Tipos/2. Solicitando dados.py
@@ -0,0 +1,9 @@
+import os;
+
+nome = str(input('Digite seu nome: '));
+idade = int(input('Digite sua idade: '));
+peso = float(input('Digite seu peso: '));
+
+print('Seu nome é:', nome);
+print('Sua idade é:', idade);
+print('Seu peso é:', peso);
---
+++ logica-de-programacao/python-exercicios/ex-sala-de-aula/aula01 - Tipos/4. Solicitando dados estaticos.py
@@ -0,0 +1,12 @@
+nome = str(input('Digite seu nome: '));
+idade = int(input('Digite sua idade: '));
+peso = float(input('Digite seu peso: '));
+
+print(f'Nome: {nome}');
+print(f'Idade: {idade}');
+print(f'Peso: {peso:.2f}');
+
+
+matriz = [[3, 4, 5],
+          [5, 6, 7],
+          [8, 9, 1]];
